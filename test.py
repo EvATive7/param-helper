cee564cbaea9526c80647fe7e2044acbec9b2cc8
EvATive7 <evative7@gmail.com>
2024-03-22 05:03:21 +0800

    feat: v1

---
+++ test.py
@@ -0,0 +1,12 @@
+
+from param_helper import param
+
+
+def test_func(phone: str, password: str, channel=0):
+    print(param(type=1))  # Additional parameters
+    ...
+
+
+test_func('11122223333', '123456', 0)
+test_func('11122223333', '123456', '1')  # Automatic type conversion based on the default type
+test_func(11122223333, '123456', 0)  # Automatic type conversion based on the annotation
