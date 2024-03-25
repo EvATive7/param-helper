d04e881ec2cc76fd96f326dd27d58b448febab89
EvATive7 <evative7@gmail.com>
2024-03-25 10:03:21 +0800

    chore: update test

--- test.py
+++ test.py
@@ -2,11 +2,11 @@
 from param_helper import param
 
 
-def test_func(phone: str, password: str, channel=0):
+def test_func(phone: str, password: str, action: list[str], channel=0):
     print(param(type=1))  # Additional parameters
     ...
 
 
-test_func('11122223333', '123456', None)
-test_func('11122223333', '123456', '1')  # Automatic type conversion based on the default type
-test_func(11122223333, '123456', 0)  # Automatic type conversion based on the annotation
+test_func('11122223333', '123456', ['a'], 1)  # default usage
+test_func('11122223333', '123456', ['a'], None)  # auto hide None
+test_func(11122223333, '123456', 'a', '0')  # automatic type conversion based on the default type and the annotation (only for types which constructor available)
