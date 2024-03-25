7e1cf5ef0d4301d0fd145304f7a983ff0a584073
EvATive7 <evative7@gmail.com>
2024-03-25 10:14:39 +0800

    chore: update test.py

--- test.py
+++ test.py
@@ -3,10 +3,19 @@ from param_helper import param
 
 
 def test_func(phone: str, password: str, action: list[str], channel=0):
-    print(param(type=1))  # Additional parameters
+    origin_vars = locals().copy()
+    param_vars = param(type=1)  # Additional parameters
+
+    print(f"""{origin_vars}
+->
+{param_vars}
+
+""")
+
+    # You can view vars in debugging
     ...
 
 
 test_func('11122223333', '123456', ['a'], 1)  # default usage
-test_func('11122223333', '123456', ['a'], None)  # auto hide None
-test_func(11122223333, '123456', 'a', '0')  # automatic type conversion based on the default type and the annotation (only for types which constructor available)
+test_func('11122223333', '123456', ['a'], None)  # Automatically hide fields with a value of None
+test_func(11122223333, '123456', 'a', '0')  # Automatic type conversion based on default value and annotation (only for types where constructors are available)
